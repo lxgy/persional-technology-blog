1556df73c5518b94a965f47bfa4c897489040aa9
lxgy <lxgy201804@163.com>
2018-12-19 16:33:36 +0800

    项目创建，框架搭建

---
+++ blog-view-web/src/main/java/com/lxgy/blogviewweb/BlogViewWebApplication.java
@@ -0,0 +1,14 @@
+package com.lxgy.blogviewweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogViewWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogViewWebApplication.class, args);
+    }
+
+}
+
